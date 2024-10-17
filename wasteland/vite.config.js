242997b478c932c5bad2d9ba2ca45dac94d181f7
Ferretsroq <Ferretsroq@gmail.com>
2024-10-17 17:51:27 -0400

    adding base variable

--- wasteland/vite.config.js
+++ wasteland/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://ferretsroq.github.io/salvage-union-wasteland/",
   plugins: [
     vue(),
   ],
